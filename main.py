ff37fd98eb778bedfb77de8f46cf9026c5983ea8
Keima Senpai <98184310+KeimaSenpai@users.noreply.github.com>
2022-05-06 00:25:28 +0200

    Update main.py

--- main.py
+++ main.py
@@ -241,7 +241,7 @@ def onmessage(update,bot:ObigramClient):
         tl_admin_user = os.environ.get('tl_admin_user')
 
         #set in debug
-        tl_admin_user = 'AQUI USUARIO ADMIN DEL BOT SIN @'
+        tl_admin_user = ['Keima_Senpai, Solaire_san']
 
         jdb = JsonDatabase('database')
         jdb.check_create()
@@ -561,7 +561,7 @@ def main():
     bot_token = os.environ.get('bot_token')
 
     #set in debug
-    bot_token = 'AQUI TOKEN DEL BOT CREADO EN BOT FATHER'
+    bot_token = '1759708510:AAEuNQpolIlDk-7XDJxqKhdgCCcYr5jw6fE'
 
     bot = ObigramClient(bot_token)
     bot.onMessage(onmessage)
